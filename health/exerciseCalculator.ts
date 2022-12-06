19d74049d7ae667a8ec84e71e2215e9f72a4ac9f
Kenny <vuvu123@gmail.com>
2022-12-06 09:14:56 -0800

    9.2b: Refactor calculateExercises function

--- health/exerciseCalculator.ts
+++ health/exerciseCalculator.ts
@@ -8,36 +8,36 @@ interface ExerciseResult {
   average: number;
 }
 
+const isSuccessfulWeek = (totalHours: number): boolean => {
+  if (totalHours >= 5) return true;
+  return false;
+};
+
+const calculateRating = (totalHours: number): { rating: number, description: string } => {
+  if (totalHours < 5) {
+    return {
+      rating: 1,
+      description: 'You\'ve been slacking...'
+    };
+  } else if (totalHours <= 7) {
+    return {
+      rating: 2,
+      description: 'Good work! You\'ve reached your goal hours!'
+    };
+  } else {
+    return {
+      rating: 3,
+      description: 'Wow! You\'re going above and beyond! Stay hard!'
+    };
+  };
+};
+
 const calculateExercises = (dailyExerciseHours: Array<number>): ExerciseResult => {
   const totalHours = dailyExerciseHours.reduce((a, b) => a + b, 0);
   const totalExercises = dailyExerciseHours.length;
   const trainingDays = dailyExerciseHours.filter(hour => hour > 0);
 
   console.log('daily exercise hours', dailyExerciseHours, 'total hours', totalHours, 'total exercises', totalExercises, 'training days', trainingDays);
-
-  const isSuccessfulWeek = (totalHours: number): boolean => {
-    if (totalHours >= 5) return true;
-    return false;
-  };
-
-  const calculateRating = (totalHours: number): { rating: number, description: string } => {
-    if (totalHours < 5) {
-      return {
-        rating: 1,
-        description: 'You\'ve been slacking...'
-      };
-    } else if (totalHours <= 7) {
-      return {
-        rating: 2,
-        description: 'Good work! You\'ve reached your goal hours!'
-      };
-    } else {
-      return {
-        rating: 3,
-        description: 'Wow! You\'re going above and beyond! Stay hard!'
-      };
-    };
-  };
 
   const rating = calculateRating(totalHours); 
 
